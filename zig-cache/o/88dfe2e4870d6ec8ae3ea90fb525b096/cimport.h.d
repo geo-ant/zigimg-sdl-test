9cdea34c4c79a356e48b1fd801e53dc363b9a99a
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-07-08 21:12:46 +0200

    initial draft based on https://dev.to/fabioarnold/setup-zig-for-gamedev-2bmf

---
+++ zig-cache/o/88dfe2e4870d6ec8ae3ea90fb525b096/cimport.h.d
@@ -0,0 +1,154 @@
+cimport.o: \
+  /home/georgios/code/zigimg-sdl-test/zig-cache/o/88dfe2e4870d6ec8ae3ea90fb525b096/cimport.h \
+  /usr/include/SDL2/SDL.h /usr/include/SDL2/SDL_main.h \
+  /usr/include/SDL2/SDL_stdinc.h /usr/include/SDL2/SDL_config.h \
+  /usr/include/SDL2/SDL_platform.h /usr/include/SDL2/begin_code.h \
+  /usr/include/SDL2/close_code.h /usr/include/sys/types.h \
+  /usr/include/features.h /usr/include/stdc-predef.h \
+  /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+  /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+  /usr/include/gnu/stubs-64.h /usr/include/bits/types.h \
+  /usr/include/bits/timesize.h /usr/include/bits/typesizes.h \
+  /usr/include/bits/time64.h /usr/include/bits/types/clock_t.h \
+  /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
+  /usr/include/bits/types/timer_t.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/stddef.h \
+  /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+  /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+  /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+  /usr/include/sys/select.h /usr/include/bits/select.h \
+  /usr/include/bits/types/sigset_t.h \
+  /usr/include/bits/types/__sigset_t.h \
+  /usr/include/bits/types/struct_timeval.h \
+  /usr/include/bits/types/struct_timespec.h \
+  /usr/include/bits/pthreadtypes.h \
+  /usr/include/bits/thread-shared-types.h \
+  /usr/include/bits/pthreadtypes-arch.h /usr/include/bits/struct_mutex.h \
+  /usr/include/bits/struct_rwlock.h /usr/include/stdio.h \
+  /usr/include/bits/libc-header-start.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/stdarg.h \
+  /usr/include/bits/types/__fpos_t.h \
+  /usr/include/bits/types/__mbstate_t.h \
+  /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+  /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+  /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
+  /usr/include/bits/floatn-common.h /usr/include/bits/stdio.h \
+  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+  /usr/include/bits/waitstatus.h /usr/include/alloca.h \
+  /usr/include/bits/stdlib-bsearch.h /usr/include/bits/stdlib-float.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/__stddef_max_align_t.h \
+  /usr/include/string.h /usr/include/bits/types/locale_t.h \
+  /usr/include/bits/types/__locale_t.h /usr/include/strings.h \
+  /usr/include/wchar.h /usr/include/bits/wchar.h \
+  /usr/include/bits/types/wint_t.h /usr/include/bits/types/mbstate_t.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/inttypes.h \
+  /usr/include/inttypes.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/stdint.h \
+  /usr/include/stdint.h /usr/include/bits/stdint-uintn.h \
+  /usr/include/ctype.h /usr/include/math.h \
+  /usr/include/bits/math-vector.h \
+  /usr/include/bits/libm-simd-decl-stubs.h \
+  /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+  /usr/include/bits/fp-fast.h \
+  /usr/include/bits/mathcalls-helper-functions.h \
+  /usr/include/bits/mathcalls.h /usr/include/SDL2/SDL_assert.h \
+  /usr/include/SDL2/SDL_atomic.h /usr/include/SDL2/SDL_audio.h \
+  /usr/include/SDL2/SDL_error.h /usr/include/SDL2/SDL_endian.h \
+  /usr/include/SDL2/SDL_mutex.h /usr/include/SDL2/SDL_thread.h \
+  /usr/include/SDL2/SDL_rwops.h /usr/include/SDL2/SDL_clipboard.h \
+  /usr/include/SDL2/SDL_cpuinfo.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/immintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/x86gprintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/hresetintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/uintrintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/mmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/xmmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/mm_malloc.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/emmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/pmmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/tmmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/smmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/popcntintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/wmmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/__wmmintrin_aes.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/__wmmintrin_pclmul.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/clflushoptintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/clwbintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avxintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx2intrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/f16cintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/vpclmulqdqintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/bmiintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/bmi2intrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/lzcntintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/fmaintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512fintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512bwintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512bitalgintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512cdintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vpopcntdqintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vpopcntdqvlintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vnniintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlvnniintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avxvnniintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512dqintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlbitalgintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlbwintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlcdintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vldqintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512erintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512ifmaintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512ifmavlintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vbmiintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vbmivlintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vbmi2intrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlvbmi2intrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512pfintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512bf16intrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlbf16intrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/pkuintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/vaesintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/gfniintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/rtmintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/xtestintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/shaintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/fxsrintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/xsaveintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/xsaveoptintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/xsavecintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/xsavesintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/cetintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/adxintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/rdseedintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/wbnoinvdintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/cldemoteintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/waitpkgintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/movdirintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/pconfigintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/sgxintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/ptwriteintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/invpcidintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/keylockerintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/amxintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vp2intersectintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/avx512vlvp2intersectintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/enqcmdintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/serializeintrin.h \
+  /var/lib/snapd/snap/zig/3852/lib/include/tsxldtrkintrin.h \
+  /usr/include/SDL2/SDL_events.h /usr/include/SDL2/SDL_video.h \
+  /usr/include/SDL2/SDL_pixels.h /usr/include/SDL2/SDL_rect.h \
+  /usr/include/SDL2/SDL_surface.h /usr/include/SDL2/SDL_blendmode.h \
+  /usr/include/SDL2/SDL_keyboard.h /usr/include/SDL2/SDL_keycode.h \
+  /usr/include/SDL2/SDL_scancode.h /usr/include/SDL2/SDL_mouse.h \
+  /usr/include/SDL2/SDL_joystick.h \
+  /usr/include/SDL2/SDL_gamecontroller.h /usr/include/SDL2/SDL_sensor.h \
+  /usr/include/SDL2/SDL_quit.h /usr/include/SDL2/SDL_gesture.h \
+  /usr/include/SDL2/SDL_touch.h /usr/include/SDL2/SDL_filesystem.h \
+  /usr/include/SDL2/SDL_haptic.h /usr/include/SDL2/SDL_hints.h \
+  /usr/include/SDL2/SDL_loadso.h /usr/include/SDL2/SDL_log.h \
+  /usr/include/SDL2/SDL_messagebox.h /usr/include/SDL2/SDL_metal.h \
+  /usr/include/SDL2/SDL_power.h /usr/include/SDL2/SDL_render.h \
+  /usr/include/SDL2/SDL_shape.h /usr/include/SDL2/SDL_system.h \
+  /usr/include/SDL2/SDL_timer.h /usr/include/SDL2/SDL_version.h \
+  /usr/include/SDL2/SDL_locale.h /usr/include/SDL2/SDL_misc.h
