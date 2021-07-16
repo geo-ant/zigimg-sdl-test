14b201d9ce38ca12361f4dd915b9a16f31109b4c
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-07-16 19:57:48 +0200

    change to use the high level Image API of zigimg

--- src/main.zig
+++ src/main.zig
@@ -55,25 +55,27 @@ pub fn main() anyerror!void {
     }
     const dst_rect = sdl2.SDL_Rect{.x=0,.y=0,.w=bitmap.width(),.h=bitmap.height()};
 
-    var png_file = try openFile(allocator, "assets/png_image.png");
-    defer png_file.close();
+    // var png_file = try openFile(allocator, "assets/png_image.png");
+    // defer png_file.close();
 
-    stream_source = std.io.StreamSource{.file = png_file};
-    var png = zigimg.png.PNG.init(allocator);
-    defer png.deinit();
+    // stream_source = std.io.StreamSource{.file = png_file};
+    // var png = zigimg.png.PNG.init(allocator);
+    // defer png.deinit();
 
-    var maybe_png_pixels: ?zigimg.color.ColorStorage = null; 
-    defer {
-        if (maybe_png_pixels) |pixels| {
-            pixels.deinit(allocator);
-        }
-    }
+    // var maybe_png_pixels: ?zigimg.color.ColorStorage = null; 
+    // defer {
+    //     if (maybe_png_pixels) |pixels| {
+    //         pixels.deinit(allocator);
+    //     }
+    // }
 
 
-    try png.read(stream_source.reader(), stream_source.seekableStream(), &maybe_png_pixels);
-    const png_texture = (try sdlTextureFromData(renderer.?,& maybe_png_pixels.?,.{.w=@intCast(i32,png.header.width), .h = @intCast(i32,png.header.height)},pixelmask));
+    // try png.read(stream_source.reader(), stream_source.seekableStream(), &maybe_png_pixels);
+    // const png_texture = (try sdlTextureFromData(renderer.?,& maybe_png_pixels.?,.{.w=@intCast(i32,png.header.width), .h = @intCast(i32,png.header.height)},pixelmask));
 
-    _ = png_texture;
+    var png_image =  try zigimg.image.Image.fromFilePath(allocator, "assets/windows_rgba_v5.bmp");
+    const png_texture = try sdlTextureFromImage(renderer.?,png_image);
+    defer sdl2.SDL_DestroyTexture(png_texture);
 
 
     mainloop: while (true) {
@@ -109,7 +111,7 @@ const ImgData = struct {
                  return @This(){.data_ptr = rgb24.ptr, .bits = 24, .pitch = 3*img_width };
              },
              else => {
-                 return error.UnsupportedColorFormat;
+                 return error.InvalidColorStorage;
              }
          }
     }
@@ -143,6 +145,79 @@ fn sdlTextureFromData(renderer: * sdl2.SDL_Renderer, storage : *zigimg.color.Col
     return texture;
 }
 
+fn unwrap(optional : anytype) !@TypeOf(optional.?) {
+    if (optional) |value| {
+        return value;
+    } else {
+        return error.EmptyOptionalUnwrap;
+    }
+}
+
+const PixelInfo = struct {
+    bits : i32,
+    pitch : i32,
+    pixelmask : PixelMask,
+
+    const Self = @This();
+
+    pub fn from(image : zigimg.image.Image) !Self {
+        const Sizes = struct {bits : u32, pitch : usize};
+        const sizes : Sizes = switch( try unwrap(image.pixels) ) {
+            .Argb32 =>  Sizes{.bits = 32, .pitch= 4*image.width},
+            .Rgb24 =>   Sizes{.bits = 24,  .pitch = 3*image.width},
+            else => return error.InvalidColorStorage,
+        };
+        return Self {
+            .bits = @intCast(i32,sizes.bits),
+            .pitch = @intCast(i32,sizes.pitch),
+            .pixelmask = try PixelMask.fromColorStorage(try unwrap(image.pixels))
+        };
+    }
+
+};
+
+
+fn sdlTextureFromImage(renderer: * sdl2.SDL_Renderer, image : zigimg.image.Image) ! ?*sdl2.SDL_Texture {
+    
+
+
+    const pxinfo = try PixelInfo.from(image);
+
+    // const data : *c_void = if (image.pixels) |storage| {
+    //     switch(storage) {
+    //         .Argb32 => |argb32| @ptrCast(*c_void,argb32.ptr),
+    //         .Rgb24 => |rgb24| @ptrCast(*c_void,rgb24.ptr),
+    //         else => return error.InvalidColorStorage,
+    //     }
+    // } else {
+    //     return error.EmptyColorStorage;
+    // };
+    const data : ?*c_void = image.pixels.?.Argb32.ptr;
+
+    const surface =  sdl2.SDL_CreateRGBSurfaceFrom(
+        data,
+        @intCast(c_int,image.width),
+        @intCast(c_int,image.height),
+        pxinfo.bits,
+        pxinfo.pitch,
+        pxinfo.pixelmask.red,
+        pxinfo.pixelmask.green,
+        pxinfo.pixelmask.blue,
+        pxinfo.pixelmask.alpha);
+    if(surface == null) {
+        return error.CreateRgbSurface;
+    }
+    defer sdl2.SDL_FreeSurface(surface);
+
+    var texture = sdl2.SDL_CreateTextureFromSurface(renderer,surface);
+    if (texture == null) {
+        return error.CreateTexture;
+    }
+
+    return texture;
+
+}
+
 const Dimensions = struct {
     w: i32, 
     h : i32
@@ -154,6 +229,26 @@ const PixelMask = struct {
     green : u32,
     blue : u32,
     alpha : u32,
+
+    const Self = @This();
+
+    pub fn fromColorStorage(storage : zigimg.color.ColorStorage) !Self {
+        switch(storage) {
+            .Argb32 => return Self {
+                .red   = 0x00ff0000,
+                .green = 0x0000ff00,
+                .blue  = 0x000000ff,
+                .alpha = 0xff000000,
+                },
+            .Rgb24 => return Self {
+                .red   = 0xff0000,
+                .green = 0x00ff00,
+                .blue  = 0x0000ff,
+                .alpha = 0,
+                },
+            else => return error.InvalidColorStorage,
+        }
+    }
 };
 
 fn extractPixelmask(bmp : *const zigimg.bmp.Bitmap) !PixelMask {
